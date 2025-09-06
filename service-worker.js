5a0c3d95911df771399de04a48bf2597557d53af
maruyaofficial-code <maruyaofficial@gmail.com>
2025-09-07 05:51:42 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "rider-app-v1";
+const FILES_TO_CACHE = [
+  "/customer.html",
+  "/rider.html",
+  "/css/style.css",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
